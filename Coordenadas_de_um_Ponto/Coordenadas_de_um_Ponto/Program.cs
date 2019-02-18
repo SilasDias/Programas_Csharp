2822f06e46fe5598c0cc6a0d9b2a7214d097401b
Silas DIas <silasdjd@gmail.com>
2019-02-18 20:01:26 -0300

    Commit Coordenadas_de_um_Ponto

---
+++ Coordenadas_de_um_Ponto/Coordenadas_de_um_Ponto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Coordenadas_de_um_Ponto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            double x = double.Parse(vet[0],CultureInfo.InvariantCulture);
+            double y = double.Parse(vet[1],CultureInfo.InvariantCulture);
+
+            if(x != 0 && y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }else if(x == 0 && y != 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }else if(x >0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }else if(x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }else if(x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }else if(x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else
+            {
+                Console.WriteLine("Origem");
+            }
+            Console.ReadLine();
+        }
+    }
+}
