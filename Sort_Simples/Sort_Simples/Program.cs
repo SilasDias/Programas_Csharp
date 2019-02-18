39865af42c0e636afefe031cd564f39fe4f08bfe
Silas DIas <silasdjd@gmail.com>
2019-02-18 20:28:29 -0300

    Commit Sort_Simples

---
+++ Sort_Simples/Sort_Simples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+            int c = int.Parse(vet[2]);
+
+            if(a < b && a < c && b < c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+            }else if(a < b && a < c && c < b)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+            }else if(b < a && b < c && a < c)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+            }else if (b < a && b < c && c < a)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+            }else if (c < a && c < b && a < b)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }else if (c < a && c < b && b < a)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sort_Simples/Sort_Simples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Sort_Simples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Sort_Simples")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("87a23c04-6ec1-4508-846e-0a5b2c69cbf1")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
