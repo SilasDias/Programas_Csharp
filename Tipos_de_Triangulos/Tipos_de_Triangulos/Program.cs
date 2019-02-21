4d6c30a386889cefecdad080dc8fee2a5c257eec
Silas DIas <silasdjd@gmail.com>
2019-02-20 21:04:14 -0300

    Estrutura Simples de Verificação. Erro Triangulo Equi e Isos

--- Tipos_de_Triangulos/Tipos_de_Triangulos/Program.cs
+++ Tipos_de_Triangulos/Tipos_de_Triangulos/Program.cs
@@ -11,25 +11,67 @@ namespace Tipos_de_Triangulos
     {
         static void Main(string[] args)
         {
-            double x, y, z, a, b, c;
+            double  a, b, c, temp;
             string[] vet;
             vet = Console.ReadLine().Split(' ');
             a = double.Parse(vet[0], CultureInfo.InvariantCulture);
             b = double.Parse(vet[1], CultureInfo.InvariantCulture);
             c = double.Parse(vet[2], CultureInfo.InvariantCulture);
             
-
-            if (a > b && a > c && b > c)
+            if (a < b)
             {
-                Console.WriteLine(a);
-                Console.WriteLine(b);
-                Console.WriteLine(c);
+                temp = a;
+                a = b;
+                b = temp;
             }
-            else if (a > b && a > c && c > b)
+            if (b < c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            if (a >= b + c)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }else if(a * a == b * b + c * c)
+            {
+                Console.WriteLine("TRIANGULO RETANGULO");
+            }else if(a * a > b * b + c * c)
+            {
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            }else if(a * a < b * b + c * c)
+            {
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+            }else if(a == b && b == c)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            }else if(a == b || b == c)
+            {
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            }
+
+           /* if (a > b && a > c && b > c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                x = (double)a;
+                y = (double)b;
+                z = (double)c;
+            }else if (a > b && a > c && c > b)
             {
                 Console.WriteLine(a);
                 Console.WriteLine(c);
                 Console.WriteLine(b);
+                x = (double)a;
+                y = (double)c;
+                z = (double)b;
             }
             else if (b > a && b > c && a > c)
             {
@@ -66,9 +108,8 @@ namespace Tipos_de_Triangulos
                 x = (double)c;
                 y = (double)b;
                 z = (double)a;
-            }
-
-                Console.ReadLine();
+            }*/
+            Console.ReadLine();
         }
     }
 }
