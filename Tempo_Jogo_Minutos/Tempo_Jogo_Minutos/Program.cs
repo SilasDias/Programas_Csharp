3edaf7dcb9dd073cf7e3e48aee72a57383838093
Silas DIas <silasdjd@gmail.com>
2019-02-25 21:39:55 -0300

    Commit Pre laço

--- Tempo_Jogo_Minutos/Tempo_Jogo_Minutos/Program.cs
+++ Tempo_Jogo_Minutos/Tempo_Jogo_Minutos/Program.cs
@@ -19,19 +19,27 @@ namespace Tempo_de_Jogo_Minutos
             fim = int.Parse(vet[2]);
             minutofinal = int.Parse(vet[3]);
 
-            tempo = fim - inicio;
-            minutos = minutofinal - minutoinicial;
-
-            if (minutos >= 1 && tempo <= 24)
+            tempo = inicio - fim;
+            if(tempo < 0)
             {
-                Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S) E " + minutos + " MINUTO(S)");
+                tempo = 24 + (inicio - fim);
             }
-            else if (tempo < 0 || tempo == 0 && minutos < 0 || minutos == 0)
+            minutos = minutoinicial - minutofinal;
+            if(minutos < 0)
             {
-                    tempo = 24 + (fim - inicio);
-                    minutos = 60 + (minutofinal - minutoinicial);
-                    Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S) E " + minutos + " MINUTO(S)");
-                }
+                minutos = 60 + (minutoinicial - minutofinal);
+                tempo--;
+            }
+
+            if (inicio == fim && minutoinicial == minutofinal)
+            {
+                Console.WriteLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+            }
+            else
+            {
+                Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S) E " + minutos+ " MINUTO(S)");
+            }
+           
             Console.ReadLine();
         }
     }
