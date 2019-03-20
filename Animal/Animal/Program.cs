fa78620d64304ca360cbeb1b3b67b2171d6c4e32
Silas DIas <silasdjd@gmail.com>
2019-03-19 21:49:49 -0300

    Commit Final

---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c, cadeia;
+
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = Console.ReadLine();
+
+            if (a == "vertebrado")
+            {
+                if (b == "mamifero")
+                {
+                    if(c == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+
+                }
+                else
+                {
+                    if(c == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+
+            }
+            else
+            {
+                if(b == "inseto")
+                {
+                    if(c == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("largata");
+                    }
+                }
+                else
+                {
+                    if(c == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
