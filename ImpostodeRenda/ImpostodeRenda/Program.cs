51ca7f93bff21595b6afdf2e04305bdbf7a74747
Silas DIas <silasdjd@gmail.com>
2019-03-21 22:54:38 -0300

    Commit Estrutura Correta

--- ImpostodeRenda/ImpostodeRenda/Program.cs
+++ ImpostodeRenda/ImpostodeRenda/Program.cs
@@ -18,34 +18,19 @@ namespace ImpostodeRenda
             }
             else if (salario > 2000.00 && salario <= 3000.00)
             {
-                Console.WriteLine("R$ " +((salario*08)/100)+salario).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("R$ " +(salario*08)/100+salario.ToString("F2", CultureInfo.InvariantCulture));
             }
             else if (salario > 800.00 && salario <= 1200.00)
             {
-                juros = 10;
-                ajuste = (salario * 10) / 100;
-                total = salario + ajuste;
-                Console.WriteLine("Novo salario: " + total.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Reajuste ganho: " + ajuste.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Em percentual: " + juros + " %");
+                Console.WriteLine("R$ " + (salario * 08) / 100 + salario.ToString("F2", CultureInfo.InvariantCulture));
             }
             else if (salario > 1200.00 && salario <= 2000.00)
             {
-                juros = 07;
-                ajuste = (salario * 07) / 100;
-                total = salario + ajuste;
-                Console.WriteLine("Novo salario: " + total.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Reajuste ganho: " + ajuste.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Em percentual: " + juros + " %");
+                Console.WriteLine("R$ " + (salario * 08) / 100 + salario.ToString("F2", CultureInfo.InvariantCulture));
             }
             else if (salario > 2000.00)
             {
-                juros = 04;
-                ajuste = (salario * 04) / 100;
-                total = salario + ajuste;
-                Console.WriteLine("Novo salario: " + total.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Reajuste ganho: " + ajuste.ToString("F2", CultureInfo.InvariantCulture));
-                Console.WriteLine("Em percentual: " + juros + " %");
+                Console.WriteLine("R$ " + (salario * 08) / 100 + salario.ToString("F2", CultureInfo.InvariantCulture));
             }
             else
             {
