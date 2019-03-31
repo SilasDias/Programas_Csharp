6d60b4cf6467b4b9e6e93806041ba85f4088b852
Silas DIas <silasdjd@gmail.com>
2019-03-30 22:09:59 -0300

    Primeiro Commit

---
+++ Senha_Fixa/Senha_Fixa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senha_Fixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            if(senha == 2002)
+            {
+                Console.WriteLine("Acesso Permitido");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
