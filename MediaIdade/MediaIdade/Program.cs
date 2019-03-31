df992eb0c6a828e55a41b3ab3068e69a5e3b0350
Silas DIas <silasdjd@gmail.com>
2019-03-30 21:57:55 -0300

    Primeiro Commit

---
+++ MediaIdade/MediaIdade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MediaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont;
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            cont = 0;
+            while (idade >= 0)
+            {
+                soma = soma + idade;
+                cont++;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine(" ");
+            media = soma / cont;
+            Console.WriteLine(media);
+        }
+    }
+}
