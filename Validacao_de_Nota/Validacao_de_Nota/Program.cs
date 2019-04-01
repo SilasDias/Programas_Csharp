518e441f68d52632b52129611dc5794dff1537dd
Silas DIas <silasdjd@gmail.com>
2019-03-31 23:24:06 -0300

    Commit Final

--- Validacao_de_Nota/Validacao_de_Nota/Program.cs
+++ Validacao_de_Nota/Validacao_de_Nota/Program.cs
@@ -13,17 +13,28 @@ namespace Validacao_de_Nota
         {
             double nota1, nota2, media;
 
-            nota1 = double.Parse(Console.ReadLine());
-            nota2 = double.Parse(Console.ReadLine());
+            nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-            while (nota1 >= 0 && nota1 <= 10 || nota2 >= 0 && nota2 <= 10)
-            {
-                media = (nota1 + nota2) / 2;
-                Console.WriteLine("media = "+media.ToString("F2",CultureInfo.InvariantCulture));
-            }
-            if(nota1 < 0 && nota1 > 10 || nota2 < 0 && nota2 > 10)
+            media = 0.0;
+            nota2 = 0.0;
+            while (nota1 < 0.0 || nota1 > 10.0)
             {
                 Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (nota2 < 0.0 || nota2 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = (nota1 + nota2) / 2;
+
+            if (media != 0.0) { 
+            Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
             }
             Console.ReadLine();
         }
