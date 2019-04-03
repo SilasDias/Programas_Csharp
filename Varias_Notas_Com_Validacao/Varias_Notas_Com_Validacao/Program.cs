5168d9ee9fd4121174cd7e76d26e1159de23d78f
Silas DIas <silasdjd@gmail.com>
2019-04-02 21:51:21 -0300

    Commit Final

--- Varias_Notas_Com_Validacao/Varias_Notas_Com_Validacao/Program.cs
+++ Varias_Notas_Com_Validacao/Varias_Notas_Com_Validacao/Program.cs
@@ -14,36 +14,43 @@ namespace Varias_Notas_Com_Validacao
             double nota1, nota2, media;
             int x;
 
-            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            x = 0;
-
-            while(x != 2) { 
-
-            media = 0.0;
-            while (nota1 < 0.0 || nota1 > 10.0)
+            x = 1;
+            do
             {
-                Console.WriteLine("nota invalida");
+                if(x == 1) { 
                 nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
 
-            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                media = 0.0;
+                while (nota1 < 0.0 || nota1 > 10.0)
+                {
+                    Console.WriteLine("nota invalida");
+                    nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
 
-            while (nota2 < 0.0 || nota2 > 10.0)
-            {
-                Console.WriteLine("nota invalida");
                 nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
 
-            media = (nota1 + nota2) / 2;
+                while (nota2 < 0.0 || nota2 > 10.0)
+                {
+                    Console.WriteLine("nota invalida");
+                    nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
 
-            if (media != 0.0)
-            {
-                Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
-            }
-            Console.WriteLine("novo calculo (1-sim 2-nao)");
-            x = int.Parse(Console.ReadLine());
-            }
+                media = (nota1 + nota2) / 2;
+
+                if (media != 0.0)
+                {
+                    Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+                    x = 2;
+                }
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    x = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    x = int.Parse(Console.ReadLine());
+                }
+            } while (x != 2);
             Console.ReadLine();
         }
     }
