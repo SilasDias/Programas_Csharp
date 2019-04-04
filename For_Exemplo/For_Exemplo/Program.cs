3c34427a83325faf34b4a563e5bfca58f0bfd89f
Silas DIas <silasdjd@gmail.com>
2019-04-03 22:20:40 -0300

    Commit Final

---
+++ For_Exemplo/For_Exemplo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, cont, soma;
+
+            n = int.Parse(Console.ReadLine());
+            soma = 0;
+            for (cont=0;cont<n;cont++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma +x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();            
+        }
+    }
+}
