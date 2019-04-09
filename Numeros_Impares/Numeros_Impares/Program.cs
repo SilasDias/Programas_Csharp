d8e99df2e63f8c74d677d79c4687155039504159
Silas DIas <silasdjd@gmail.com>
2019-04-09 16:31:29 -0300

    Commit Final

---
+++ Numeros_Impares/Numeros_Impares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_Impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, i, impar;
+
+            x = int.Parse(Console.ReadLine());
+
+            for(i=0; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
