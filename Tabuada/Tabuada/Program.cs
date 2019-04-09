11e3e7111731465a8d11685fc1ec9423b0c987c5
Silas DIas <silasdjd@gmail.com>
2019-04-09 14:16:21 -0300

    Commit Concluido

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+
+            n = int.Parse(Console.ReadLine());
+
+            if( n >= 2 && n <= 1000)
+            {
+                for(i=1; i <= 10; i++)
+                {
+                    Console.WriteLine(i + " x " + n + " = " + i * n);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
