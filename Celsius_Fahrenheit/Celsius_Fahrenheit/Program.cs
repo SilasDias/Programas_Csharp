3e33632b3f3c33d8beca58f26161c44e51e6a870
Silas DIas <silasdjd@gmail.com>
2019-04-09 15:55:27 -0300

    Commit Conclusao

---
+++ Celsius_Fahrenheit/Celsius_Fahrenheit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Celsius_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f;
+            char escolha;
+
+            do
+            {
+                Console.WriteLine("Digite a temperatura em Celsius: ");
+                c = double.Parse(Console.ReadLine());
+                f = 9.0 * c / 5.0 + 32;
+                Console.WriteLine("Equivalente a Fahrenheit: " + f.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Deseja Repetir (S/N)?: ");
+                escolha = char.Parse(Console.ReadLine());
+            } while (escolha == 's');
+            Console.ReadLine();
+        }
+    }
+}
