49e76c383e06dcf409b26c727d94443449f70068
Silas DIas <silasdjd@gmail.com>
2019-04-11 15:13:01 -0300

    Commit Final

---
+++ Intervalo_2/Intervalo_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, i, dentro=0, fora=0;
+
+           n = int.Parse(Console.ReadLine());
+
+            for(i=0; i < n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quadrado_de_ Pares/Quadrado_de_ Pares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado_de__Pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            long n;
+
+            n = long.Parse(Console.ReadLine());
+
+            if (n > 5 && n < 2000)
+            {
+
+                for (i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i + "^2 = " + i * i);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
