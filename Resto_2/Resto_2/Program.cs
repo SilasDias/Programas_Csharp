c857af81cc88a196b3cd6d1ef3a969ba0b11de01
Silas DIas <silasdjd@gmail.com>
2019-04-11 15:46:04 -0300

    Commit Final

---
+++ Resto_2/Resto_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+
+            n = int.Parse(Console.ReadLine());
+
+            if(n < 10000)
+            {
+                for (i = 1; i <= 10000; i++)
+                {
+                    if(i % n == 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadLine();
+            }            
+        }
+    }
+}
