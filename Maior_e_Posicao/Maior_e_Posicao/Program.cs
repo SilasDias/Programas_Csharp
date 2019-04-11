1f040e9a74716acbb725a0f56e3a4176b13e3036
Silas DIas <silasdjd@gmail.com>
2019-04-11 16:52:24 -0300

    Commit Final

---
+++ Maior_e_Posicao/Maior_e_Posicao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior_e_Posicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=0, n, e=0, a=0, b, i;
+
+            for (i = 0; i < 100; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                x++;
+                if (n > e) {
+
+                    a = x;
+                    e = n;
+                }
+            }
+            Console.WriteLine(e);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Medias_Ponderadas/Medias_Ponderadas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Medias_Ponderadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                string[] vet;
+                vet = Console.ReadLine().Split(' ');
+                double x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                double y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                double z = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                double media = ((x * 2) + (y * 3) + (z * 5)) / 10;
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
