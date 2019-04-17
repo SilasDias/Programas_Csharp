cdc4768a713beefdb29e65ed0e932a308789a1ba
Silas DIas <silasdjd@gmail.com>
2019-04-16 22:41:22 -0300

    Commit Final

--- Sequencia_de_Numeros_e_Soma/Sequencia_de_Numeros_e_Soma/Program.cs
+++ Sequencia_de_Numeros_e_Soma/Sequencia_de_Numeros_e_Soma/Program.cs
@@ -16,24 +16,30 @@ namespace Sequencia_de_Numeros_e_Soma
             vet = Console.ReadLine().Split(' ');
             int x = int.Parse(vet[0]);
             int y = int.Parse(vet[1]);
-
-            if (x < y)
-            {
-                min = x;
-                max = y;
-            }
-            else
-            {
-                min = y;
-                max = x;
-            }
-            soma = 0;
-            for (i = min; i <= max; i++)
-            {
-                Console.Write(i);
-                soma = soma + i;
-            }
-            Console.WriteLine("Sum="+soma);
+            
+            do {
+               
+                if (x < y)
+                {
+                    min = x;
+                    max = y;
+                }
+                else
+                {
+                    min = y;
+                    max = x;
+                }
+                soma = 0;
+                for (i = min; i <= max; i++)
+                {
+                    Console.Write(i + " ");
+                    soma = soma + i;
+                }
+                Console.WriteLine("Sum=" + soma);
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            } while (x > 0 && y > 0);
             Console.ReadLine();
         }
     }
