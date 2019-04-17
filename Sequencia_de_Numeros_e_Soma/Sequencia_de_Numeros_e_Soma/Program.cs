be7b784d9d2056a257b7ad6414e7d98d32c9d31c
Silas DIas <silasdjd@gmail.com>
2019-04-16 22:24:15 -0300

    Commit Inicial

---
+++ Sequencia_de_Numeros_e_Soma/Sequencia_de_Numeros_e_Soma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequencia_de_Numeros_e_Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max, soma, i;
+
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            int x = int.Parse(vet[0]);
+            int y = int.Parse(vet[1]);
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+            soma = 0;
+            for (i = min; i <= max; i++)
+            {
+                Console.Write(i);
+                soma = soma + i;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
