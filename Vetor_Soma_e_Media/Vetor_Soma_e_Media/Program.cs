52b86db294c8e25cc482cd57bed093a4ef593a13
Silas DIas <silasdjd@gmail.com>
2019-05-02 14:58:16 -0300

    Commit Estrutura

---
+++ Vetor_Soma_e_Media/Vetor_Soma_e_Media/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Vetor_Soma_e_Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] a;
+            n = int.Parse(Console.ReadLine());
+            a = new double[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i=0; i<n; i++)
+            {
+                a[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            for(int i=0; i<n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+
+            double soma = 0.0;
+            for(int i=0; i<n; i++)
+            {
+                soma = soma + a[i];
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
