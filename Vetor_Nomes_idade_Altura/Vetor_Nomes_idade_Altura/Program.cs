7324e75521a48aa51c5d5ba24b59dd8d30bdcd8a
Silas DIas <silasdjd@gmail.com>
2019-05-03 16:43:19 -0300

    Leitura dos Dados

--- Vetor_Nomes_idade_Altura/Vetor_Nomes_idade_Altura/Program.cs
+++ Vetor_Nomes_idade_Altura/Vetor_Nomes_idade_Altura/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Vetor_Nomes_idade_Altura
 {
@@ -11,7 +12,22 @@ namespace Vetor_Nomes_idade_Altura
         static void Main(string[] args)
         {
             int n;
-            string[] 
+            n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+            double[] alturas = new double[n];
+
+            //Leitura dos Dados
+            for(int i=0; i<n; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2],CultureInfo.InvariantCulture );
+
+            }
+
+
         }
     }
 }
