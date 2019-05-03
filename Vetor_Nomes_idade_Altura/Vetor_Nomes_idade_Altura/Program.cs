338e6ef954f5c55a83cdea49a56f9693d722e168
Silas DIas <silasdjd@gmail.com>
2019-05-03 16:02:52 -0300

    COmmit Inicial

---
+++ Vetor_Nomes_idade_Altura/Vetor_Nomes_idade_Altura/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor_Nomes_idade_Altura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string[] 
+        }
+    }
+}
