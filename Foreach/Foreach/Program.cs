64bf52b8ec0b236e4abba77b602e0edbdd6bb7d6
Silas DIas <silasdjd@gmail.com>
2019-05-03 17:26:08 -0300

    Sem o Foreach

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            string[] vet = new string[n];
+
+            for(int i=0; i<n; i++)
+            {
+                vet[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes lidos: ");
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
