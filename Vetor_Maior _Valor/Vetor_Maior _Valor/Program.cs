0ea44b4cb9d9b2d04e550f6e5c3022fe5a89ff17
Silas DIas <silasdjd@gmail.com>
2019-05-06 20:04:17 -0300

    Estrutura

---
+++ Vetor_Maior _Valor/Vetor_Maior _Valor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor_Maior__Valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1) Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número
+            do vetor(supor não haver empates).Mostrar também a posição do maior elemento.*/
+
+            int n, x=0;
+            double[] a;
+
+            n = int.Parse(Console.ReadLine());
+            a = new double[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = double.Parse(s[i]);
+            }
+            int e = 0;
+            double valor=0;
+            for (int i = 0; i < n; i++)
+            {
+                x++;
+                if (a[i] > e)
+                {
+                    e = x;
+                    valor = (double) a[i];
+                }
+            }
+            Console.WriteLine(valor);
+            Console.WriteLine(e);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
