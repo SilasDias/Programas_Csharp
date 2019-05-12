12e242b1f180a4cedf40dbf0bdfebd9c28e2a050
Silas DIas <silasdjd@gmail.com>
2019-05-12 14:36:04 -0300

    Estrutura

---
+++ Vetor_Soma_Vetor/Vetor_Soma_Vetor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor_Soma_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] a, b, c;
+
+            n = int.Parse(Console.ReadLine());
+            a = new int[n];
+            b = new int[n];
+            c = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            string[] r = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+                b[i] = int.Parse(r[i]);
+                c[i] = (a[i]+b[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(c[i]+" ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
