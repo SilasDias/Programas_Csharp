bdc1a12535e6e7757820137920e30c981ab21965
Silas DIas <silasdjd@gmail.com>
2019-05-12 14:46:58 -0300

    Estrutura

---
+++ Vetor_Media_Numeros_Reais/Vetor_Media_Numeros_Reais/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Vetor_Media_Numeros_Reais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] vet;
+            double media=0, soma=0;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+                soma = vet[i] + soma;
+            }
+            media = soma / n;
+            Console.WriteLine(media);
+            for (int i = 0; i < n; i++)
+            {
+                if(vet[i] < media)
+                {
+                    Console.Write(vet[i] + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
